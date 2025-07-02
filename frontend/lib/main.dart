01067e8a3ea253367ad7ab45b0bc71ce8a29036f
SandChik <rohiid.naufal.tif422@polban.ac.id>
2025-07-02 13:00:40 +0700

    initial welcome screen page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:frontend/screens/home_screen.dart'; // Impor layar home kita
+=======
+import 'package:frontend/screens/home_screen.dart';
+import 'package:frontend/screens/register_screen.dart';
+import 'package:frontend/screens/welcome_screen.dart';
+>>>>>>> Stashed changes
 
 
 void main() {
@@ -17,7 +23,18 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         brightness: Brightness.dark, // Tema gelap agar keren
       ),
+<<<<<<< Updated upstream
       home: const HomeScreen(), // Layar pertama yang ditampilkan adalah HomeScreen
+=======
+      home:
+          const WelcomeScreen(), // Layar pertama yang ditampilkan adalah HomeScreen
+      routes: {
+        // Daftar route (alamat layar) yang bisa diakses di aplikasi
+        '/welcome': (context) => const WelcomeScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/register': (context) => const RegisterScreen(),
+      },
+>>>>>>> Stashed changes
     );
   }
 }
